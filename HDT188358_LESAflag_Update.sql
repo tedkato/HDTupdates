d18a2319c89fdd8bea155b6fad028ff894ddb2a0
tedkato <94704206+tedkato@users.noreply.github.com>
2025-05-02 12:18:47 -0400

    Update HDT188358_LESAflag_Update.sql
    comments removed

--- HDT188358_LESAflag_Update.sql
+++ HDT188358_LESAflag_Update.sql
@@ -1,14 +1,11 @@
--- LESA flag update
---count before
 SELECT 
 count(*) 
 from DATAUPDT.MICDateFlag UY 
 inner join celink.tblTaxAndInsData TT
 ON UY.LoanNum = TT.TIDATALOANNUMBER
 where TT.TISAMANDATORY = 58
-; --45764/0
+;
 
-			-- update
 			MERGE INTO celink.tblTaxAndInsData TT
 			USING 
 			(
@@ -23,26 +20,17 @@ where TT.TISAMANDATORY = 58
 			TT.TISAMANDATORY = 56
 			;	
 
---count after
 SELECT 
 count(*) 
 from DATAUPDT.MICDateFlag UY 
 inner join celink.tblTaxAndInsData TT
 ON UY.LoanNum = TT.TIDATALOANNUMBER
 where TT.TISAMANDATORY = 56
-; --0/45764 
+;
 
-
-----------------------------------------------------------------------------------------------------
--- Note insertion  
--- Count the number of existing notes
 select count(*) from celink.tblLoanNotes for read only with ur; 
--- 185163045
-
 		Insert into celink.tblLoanNotes 
 		(INTSUBSERVICERLOANNUMBER, CHRLOANNOTES, DTMNOTEDATE, CHRUSERID, BLNPRIORITY, NoteStep)
 		select LOAN, LOANNOTES, CURRENT_TIMESTAMP, USERID, PRIORITY, 0 from DATAUPDT.LOANNOTES_TKATO order by LOAN ;
 		
--- Count the number of existing notes again    
 select count(*) from celink.tblLoanNotes for read only with ur; 
---  185208809
