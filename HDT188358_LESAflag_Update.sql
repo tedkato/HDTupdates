33977973c4f2a2f0958e9fe7e6d7d520bccd320e
tedkato <94704206+tedkato@users.noreply.github.com>
2025-05-02 12:34:26 -0400

    Update HDT188358_LESAflag_Update.sql
    A part to note insertion removed as it has been done in the initial execution.

--- HDT188358_LESAflag_Update.sql
+++ HDT188358_LESAflag_Update.sql
@@ -6,19 +6,19 @@ ON UY.LoanNum = TT.TIDATALOANNUMBER
 where TT.TISAMANDATORY = 58
 ;
 
-			MERGE INTO celink.tblTaxAndInsData TT
-			USING 
-			(
-				SELECT
-					UY.SeqNum, 
-					UY.LoanNum
-				from DATAUPDT.MICDateFlag UY
-			) UY (SeqNum, LoanNum)
-			ON UY.LoanNum = TT.TIDATALOANNUMBER
-			WHEN MATCHED THEN UPDATE 
-			Set
-			TT.TISAMANDATORY = 56
-			;	
+MERGE INTO celink.tblTaxAndInsData TT
+USING 
+(
+SELECT
+UY.SeqNum, 
+UY.LoanNum
+from DATAUPDT.MICDateFlag UY
+) UY (SeqNum, LoanNum)
+ON UY.LoanNum = TT.TIDATALOANNUMBER
+WHEN MATCHED THEN UPDATE 
+Set
+TT.TISAMANDATORY = 56
+;	
 
 SELECT 
 count(*) 
@@ -27,10 +27,3 @@ inner join celink.tblTaxAndInsData TT
 ON UY.LoanNum = TT.TIDATALOANNUMBER
 where TT.TISAMANDATORY = 56
 ;
-
-select count(*) from celink.tblLoanNotes for read only with ur; 
-		Insert into celink.tblLoanNotes 
-		(INTSUBSERVICERLOANNUMBER, CHRLOANNOTES, DTMNOTEDATE, CHRUSERID, BLNPRIORITY, NoteStep)
-		select LOAN, LOANNOTES, CURRENT_TIMESTAMP, USERID, PRIORITY, 0 from DATAUPDT.LOANNOTES_TKATO order by LOAN ;
-		
-select count(*) from celink.tblLoanNotes for read only with ur; 
